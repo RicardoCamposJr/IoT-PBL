27261cabf620fe434595f7aecc1f6911d0d3e44d
Ricardo Campos <rickoliver001@hotmail.com>
2024-04-11 22:22:15 -0300

    ✨ feat: Improve the device logical

--- device/device.py
+++ device/device.py
@@ -2,66 +2,113 @@ import socket
 import threading
 import time
 
+comand = True
+choice = ''
+serverIP = ''
+TCP_PORT = ''
+UDP_PORT = ''
+
+socketUDP = ''
+socketTCP = ''
+
+receiverTCPThread = ''
+transmitterUDPThread = ''
+
+# Conecta o socket TCP ao broker
+def connectSocketTCP():
+    global socketTCP
+    global TCP_PORT
+
+    socketTCP.connect((serverIP, TCP_PORT))
+    print(f'Conectado ao servidor TCP em {serverIP}:{TCP_PORT}')
+
+# Cria os sockets TCP e UDP
+def createSockets():
+    global socketUDP
+    global socketTCP
+
+    # Criação do socket UDP
+    socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Criação do socket TCP/IP
+    socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Cria e roda a thread de recebimento de comandos via TCP
+def createReceiverTCPThread():
+    global receiverTCPThread
+
+    receiverTCPThread = threading.Thread(target=receiveTCPServer)
+    receiverTCPThread.start()
+
+# Cria e roda a thread de envio de dados via UDP
+def createTransmitterUDPThread():
+    global transmitterUDPThread
+
+    transmitterUDPThread = threading.Thread(target=transmitterUDPData)
+    transmitterUDPThread.start()
+
 # Função para receber comandos do servidor via TCP
-def receber_do_servidor_tcp(tcp_client):
+def receiveTCPServer():
+    global socketTCP
     while True:
-        data = tcp_client.recv(1024)
+        data = socketTCP.recv(1024)
         if not data:
             break
         print("Comando recebido do servidor TCP:", data.decode())
 
 # Função para enviar dados para o servidor via UDP
-def enviar_para_servidor_udp(udp_client):
+def transmitterUDPData():
+    global serverIP
+    global UDP_PORT
+    global socketUDP
+    global choice
+
     while True:
-        mensagem = input("Dados UDP")
-        udp_client.sendto(mensagem.encode(), (UDP_HOST, UDP_PORT))
+        # Caso o usuário escolha desligar o dispositivo
+        if (choice == 2):
+            print("\nDispositivo desligado. Envio de dados cancelado.\nInsira seu comando: ")
+            break
+        mensagem = ("Mensagem UDP")
+        socketUDP.sendto(mensagem.encode(), (serverIP, UDP_PORT))
+        time.sleep(3)
 
-# Função para receber comandos do usuário via terminal
-def receber_comandos_do_usuario():
-    while True:
-        comando = input("Digite um comando (ex: 'OFF' para encerrar): ")
-        if comando == "OFF":
-          # Encerra a conexão UDP
-          udp_client.close()
-          print('Conexão UDP encerrada')
+# Menu inicial de configuração do dispositivo
+def menuConfig():
+    global TCP_PORT
+    global UDP_PORT
+    global serverIP
 
-# Configurações do servidor UDP
-UDP_HOST = 'localhost'
-UDP_PORT = 8889
+    print("Configure seu dispositivo:")
+    serverIP = input("IP do Servidor: ")
+    UDP_PORT = int(input("Porta UDP: "))
+    TCP_PORT = int(input("Porta TCP: "))
 
-# Criação do socket UDP
-udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Controle do dispositivo pelo usuário via terminal
+def menuComand() :
+    global choice
+    global comand
+    
+    choice = int(input("\nBem vindo seu dispositivo!\n[0] - Ligar\n[1] - Definir temperatura\n[2] - Desligar\n"))
+    if choice == 0:
+        # Iniciando o envio de dados via UDP:
+        createTransmitterUDPThread()
 
-# Configurações do servidor TCP
-TCP_HOST = 'localhost'
-TCP_PORT = 8888
 
-# Criação do socket TCP/IP
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Configurando o dispositivo antes de iniciá-lo:
+menuConfig()
+createSockets()
+connectSocketTCP()
+createReceiverTCPThread()
 
-# Conecta o socket TCP ao servidor
-tcp_client.connect((TCP_HOST, TCP_PORT))
-print(f'Conectado ao servidor TCP em {TCP_HOST}:{TCP_PORT}')
 
-# Criação das threads
-thread_receber_tcp = threading.Thread(target=receber_do_servidor_tcp, args=(tcp_client,))
-thread_enviar_udp = threading.Thread(target=enviar_para_servidor_udp, args=(udp_client,))
-thread_receber_comandos = threading.Thread(target=receber_comandos_do_usuario)
+# Início da manipulação do dispositivo pelo usuário via terminal
+while (comand):
+    menuComand()
 
-# Inicia as threads
-thread_receber_tcp.start()
-thread_enviar_udp.start()
-thread_receber_comandos.start()
-
-# Aguarda o término das threads
-thread_receber_tcp.join()
-thread_enviar_udp.join()
-thread_receber_comandos.join()
 
 # Encerra a conexão TCP
-tcp_client.close()
+socketTCP.close()
 print('Conexão TCP encerrada')
 
 # Encerra a conexão UDP
-udp_client.close()
+socketUDP.close()
 print('Conexão UDP encerrada')
